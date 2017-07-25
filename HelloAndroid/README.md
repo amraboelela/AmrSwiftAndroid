bfe4eb2f09c41ad148aa62fffbaaa6fdcc41718f
Amr Aboelela <amraboelela@gmail.com>
2017-07-25 11:38:54 -0700

    Added HelloAndroid

---
+++ HelloAndroid/README.md
@@ -0,0 +1,2 @@
+# HelloAndroid
+hello world application using swift and porting it to Android
---
+++ HelloAndroid/hello.swift
@@ -0,0 +1,23 @@
+//using namespace swift
+//import swift
+// Foundation
+
+print("Hello, Android")
+
+/*
+class Hello {
+
+func printHello() {
+   // NSLog("hay")
+    //swift.getRootSuperclass()
+    //return
+    print("Hello, Android")
+}
+}
+
+func main() {
+    let hello = Hello()
+    hello.printHello()
+//printHello()
+}*/
+
