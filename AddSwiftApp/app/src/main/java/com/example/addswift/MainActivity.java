f1588c1edba99c333906ed3159351ca39726a10f
Amr Aboelela <amraboelela@gmail.com>
2017-07-26 10:51:52 -0700

    reload

--- AddSwiftApp/app/src/main/java/com/example/addswift/MainActivity.java
+++ AddSwiftApp/app/src/main/java/com/example/addswift/MainActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.hellolibs;
+package com.example.addswift;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
