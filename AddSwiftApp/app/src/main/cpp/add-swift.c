f2ae544b1810fd1d8a567e1213ac671eb1646e32
Amr Aboelela <amraboelela@gmail.com>
2017-08-23 21:09:40 -0700

    added build.sh

--- AddSwiftApp/app/src/main/cpp/add-swift.c
+++ AddSwiftApp/app/src/main/cpp/add-swift.c
@@ -2,23 +2,26 @@
 #include <jni.h>
 #include <stdint.h>
 #include <stdio.h>
+//#include "gperf.h"
 
 // Unfortunately this has to be hand-generated for now...
 //#define SWIFT_ADD _T08AddSwift13addTwoNumberss5UInt8VAD5first_AD6secondtF
 
+#define MY_MAIN _T08AddSwift6MyMainyyF
 
 //uint8_t SWIFT_ADD(uint8_t, uint8_t);
 
 // Promise C we've implemented this function.
-//extern void swiftAdd(void);
+extern void MY_MAIN(void);
 
-jstring Java_com_example_addswift_MainActivity_stringFromJNI(JNIEnv * env, jobject thiz ) {
+JNIEXPORT jstring JNICALL
+Java_com_example_addswift_MainActivity_stringFromJNI(JNIEnv * env, jobject thiz ) {
     
     uint8_t a = 123;
     uint8_t b = 45;
-    //swiftAdd();
+    MY_MAIN();
     uint8_t c = a+b; //SWIFT_ADD(a,b);
-    
+    //GetTicks();
     char result[255];
     sprintf(result, "We are computing the addition of %d and %d.\n\nThe result is %d.\n\nThis computation was written in Swift and is running on this Android device!", a, b, c);
     
